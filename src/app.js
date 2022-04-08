3f70a0312558589c6f3c45dcb8ba9eb7192fc272
Tyler <tgrande@my.bcit.ca>
2022-04-08 16:04:29 -0700

    added save and clear buttons added functionality to clear button and stopped duplication of code for clicking on button

--- src/app.js
+++ src/app.js
@@ -6,21 +6,49 @@ const notes = [
   }
 ]
 
+//create text area
+const textArea = document.createElement("textarea");
+textArea.id = 'textarea1';
+textArea.rows = 30;
+textArea.cols = 50;
+textArea.textContent = 'Write your new note here!'
+
+//create save button
+const saveBut = document.createElement("button")
+saveBut.id = 'idcancel'
+saveBut.textContent = 'Save'
+
+//create cancel button
+const canBut = document.createElement("button");
+canBut.id = 'classcancel';
+canBut.textContent = 'cancel';
+
+var textAreaEle = document.getElementById('textArea1');
+const noteLocation = document.querySelector(".create-note-area");
+const noteBtn = document.querySelector(".fa-circle-plus");
+
+function createTextBox() {
+  noteLocation.append(textArea)
+  noteLocation.append(saveBut)
+  noteLocation.append(canBut)
 
 
-const noteLocation = document.querySelector(".note-area")
-const noteBtn = document.querySelector(".fa-circle-plus")
+  noteBtn.removeEventListener("click", createTextBox)
 
-noteBtn.addEventListener("click", (evt) => {
-  var textarea = document.getElementById('textarea');
-  console.log(textarea)
-  if(textarea === null){
-    textarea = document.createElement("textarea");
-    textarea.id = 'textarea';
-    textarea.rows = 30;
-    textarea.cols = 50;
-    noteLocation.append(textarea);
-    } else{
-    console.log(notes);
-    }
+
+
+  const canButEle = document.querySelector('#classcancel')
+  canButEle.addEventListener("click", (evt) => {
+      textAreaEle = document.getElementById('textarea1')
+      saveButEle = document.querySelector('#idcancel')
+
+      canButEle.remove()
+      saveButEle.remove()
+      textAreaEle.remove()
+
+    noteBtn.addEventListener("click", createTextBox)
   })
+}
+noteBtn.addEventListener("click", createTextBox)
+
+
