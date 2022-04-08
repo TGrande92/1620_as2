9aabe1db564be41069d0986fe63b4a3f8a7dcb6a
Tyler <tgrande@my.bcit.ca>
2022-04-08 10:09:14 -0700

    Created javascript code to create text area while clicking the button and stopped it from creating more text areas after clicking the button

--- src/app.js
+++ src/app.js
@@ -5,3 +5,22 @@ const notes = [
     id: 1 
   }
 ]
+
+
+
+const noteLocation = document.querySelector(".note-area")
+const noteBtn = document.querySelector(".fa-circle-plus")
+
+noteBtn.addEventListener("click", (evt) => {
+  var textarea = document.getElementById('textarea');
+  console.log(textarea)
+  if(textarea === null){
+    textarea = document.createElement("textarea");
+    textarea.id = 'textarea';
+    textarea.rows = 30;
+    textarea.cols = 50;
+    noteLocation.append(textarea);
+    } else{
+    console.log(notes);
+    }
+  })
