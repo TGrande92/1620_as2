46727e99eaab9cd6688840f7671798ecb73838a5
Tyler <tgrande@my.bcit.ca>
2022-04-09 13:34:58 -0700

    commiting to push to work on my desktop

--- src/app.js
+++ src/app.js
@@ -22,10 +22,11 @@ const canBut = document.createElement("button");
 canBut.id = 'classcancel';
 canBut.textContent = 'cancel';
 
+//global variables
 var textAreaEle = document.getElementById('textArea1');
 const noteLocation = document.querySelector(".create-note-area");
 const noteBtn = document.querySelector(".fa-circle-plus");
-
+//textarea function and cancel function
 function createTextBox() {
   textArea.value = ''
   noteLocation.append(textArea)
@@ -39,8 +40,8 @@ function createTextBox() {
 
   const canButEle = document.querySelector('#classcancel')
   canButEle.addEventListener("click", (evt) => {
-      let textAreaEle = document.getElementById('textarea1')
-      let saveButEle = document.querySelector('#idcancel')
+      const textAreaEle = document.getElementById('textarea1')
+      const saveButEle = document.querySelector('#idcancel')
 
       canButEle.remove()
       saveButEle.remove()
@@ -50,7 +51,7 @@ function createTextBox() {
   })
 }
 noteBtn.addEventListener("click", createTextBox)
-
+// save button functions
 saveBut.addEventListener("click", (evt) => {
   let textAreaValue = noteLocation.querySelector('#textarea1').value
   textAreaValue = textAreaValue.split('\n')
@@ -65,10 +66,31 @@ saveBut.addEventListener("click", (evt) => {
   console.log(notes)
   let liAdd = document.createElement("li")
   liAdd.textContent = textAreaValue[0]
+  liAdd.id = notes.length + 1
   const notesList = document.querySelector('.notes-list')
   notesList.append(liAdd)
   noteBtn.addEventListener("click", createTextBox)
-})
+  })
 
 
+
+//open nav title
+
+const noteSelect = document.querySelectorAll(".notes-list");
+const readNote = document.querySelector(".read-note-area");
+
+noteSelect.forEach((element) =>
+  element.addEventListener("click", (evt) => {
+    listNotes = notes.map(({ noteBody }) => noteBody)
+    console.log(listNotes)
+   // idNotes = notes.map(({ id })) //change this to reference the dictionary id to compare7
+    
+    // for (let string of listNotes)
+    //   for (let item of string)
+    //     console.log(item)
+
+
+    readNote.insertAdjacentHTML("beforeend", notes[1].noteBody);
+  })
+);
 
